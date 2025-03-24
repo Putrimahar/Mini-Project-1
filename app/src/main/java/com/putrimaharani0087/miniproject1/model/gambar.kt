bf7bdc35ae32f21dd9c884f25e7eed2fcbf3fd13
Putri Maharani <maharani2005.putrii@gmail.com>
2025-03-24 18:01:38 +0700

    Membuat Tampilan Gambar dan Teks di dalam penjelasan (screen kedua)

---
+++ app/src/main/java/com/putrimaharani0087/miniproject1/model/gambar.kt
@@ -0,0 +1,7 @@
+package com.putrimaharani0087.miniproject1.model
+
+import androidx.annotation.DrawableRes
+
+data class gambar(
+    @DrawableRes val imageResId: Int
+)
--- app/src/main/java/com/putrimaharani0087/miniproject1/navigation/RumusScreen.kt
+++ app/src/main/java/com/putrimaharani0087/miniproject1/navigation/RumusScreen.kt
@@ -1,9 +1,21 @@
 package com.putrimaharani0087.miniproject1.navigation
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,18 +25,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.putrimaharani0087.miniproject1.R
+import com.putrimaharani0087.miniproject1.model.gambar
 import com.putrimaharani0087.miniproject1.ui.theme.MiniProject1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RumusScreen(navController: NavHostController) {
+    val data = gambar(R.drawable.gambar_rumus)
     Scaffold (
         topBar = {
             TopAppBar(
@@ -48,10 +65,47 @@ fun RumusScreen(navController: NavHostController) {
         }
     ) {
         innerPadding ->
-        Text(
-            text = stringResource(R.string.penjelasan),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
-        )
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(30.dp)
+                .fillMaxWidth()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = data.imageResId),
+                contentDescription = "Gambar Rumus",
+                modifier = Modifier.size(300.dp),
+            )
+            Text(
+                text = stringResource(R.string.isi_penjelasan),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.padding(12.dp))
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(R.string.rumus),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
     }
 }
 
